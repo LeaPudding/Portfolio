fcb119af448ee67d72622852763f6d79f153ac08
Philea Magno <phileacharisemagno@gmail.com>
2024-08-13 18:50:19 -0400

    Add files via upload

---
+++ Portfolio/assets/script.js
@@ -0,0 +1,28 @@
+
+
+
+// var landingPage = 'index.html';
+
+// function updateNavStyle() {
+//   if(landingPage.length > 0 && location.href.toLowerCase().indexOf(landingPage.toLowerCase()) >= 0) {
+//     var offset = $('#change_color').offset();
+//     var scroll_start = $(document).scrollTop();
+//     if (scroll_start > offset.top) {
+//       // the white normal navbar
+//       $(".navbar-add").removeClass("navbar-trans");
+//     } else {
+//       // what the users sees when he lands on the page
+//       $(".navbar-add").addClass("navbar-trans");
+//     }
+//   }
+// }
+
+// $(document).ready(function() {
+//   var scroll_start = 0;
+//   var change_color = $('#change_color');
+
+//   if (change_color.length) {
+//     $(document).scroll(updateNavStyle);
+//     updateNavStyle(); // Note this.
+//   }
+// });
